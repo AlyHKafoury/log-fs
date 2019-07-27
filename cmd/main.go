0ad230d35614dd7d05ec5f1647d160b1121e67f3
AlyHKafoury <aly.kafoury@gmail.com>
2019-07-27 21:09:39 +0000

    capture write data

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,24 @@ import (
 	"github.com/hanwen/go-fuse/fs"
 	"github.com/hanwen/go-fuse/fuse"
 	"log"
+    "os/user"
+    "strconv"
 )
 
 func main() {
-	server, err := fs.Mount("test", &filesystem.Node{}, &fs.Options{MountOptions: fuse.MountOptions{Debug: true}})
+    var uid, gid string
+    user, err := user.Current()
+    if err != nil {
+        log.Println("Can't get current user default root")
+        uid = "0"
+        gid = "0"
+    }else {
+        uid = user.Uid
+        gid = user.Gid
+    }
+    paramUID, _ := strconv.Atoi(uid)
+    paramGID, _ := strconv.Atoi(gid)
+	server, err := fs.Mount("test", &filesystem.Node{Name: "test"}, &fs.Options{MountOptions: fuse.MountOptions{Debug: true}, UID: uint32(paramUID), GID: uint32(paramGID)})
 	if err != nil {
 		log.Panic(err)
 	}
--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -5,28 +5,42 @@ import (
 	"github.com/hanwen/go-fuse/fs"
 	"github.com/hanwen/go-fuse/fuse"
 	"syscall"
+    "fmt"
+    "path/filepath"
 )
 
 type Node struct {
 	fs.Inode
+    Name string
 }
 
 var _ = (fs.InodeEmbedder)((*Node)(nil))
 var _ = (fs.NodeLookuper)((*Node)(nil))
 var _ = (fs.NodeReaddirer)((*Node)(nil))
+var _ = (fs.NodeWriter)((*Node)(nil))
 
 func (n *Node) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
-	node := Node{}
-	return n.NewInode(ctx, &node, fs.StableAttr{}), 0
+	node := Node{Name: name}
+    if filepath.Ext(name) == "" {
+        return n.NewInode(ctx, &node, fs.StableAttr{Mode: syscall.S_IFDIR}), 0
+    }
+	return n.NewInode(ctx, &node, fs.StableAttr{Mode: syscall.S_IFREG}), 0
 }
 
 func (n *Node) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
-	r := make([]fuse.DirEntry, 0, 1)
-	d := fuse.DirEntry{
-		Name: "Test file",
-		Ino:  1000,
-		Mode: fuse.S_IFREG,
-	}
-	r = append(r, d)
+	r := make([]fuse.DirEntry, 0)
 	return fs.NewListDirStream(r), 0
+}
+
+func (n *Node) Open(ctx context.Context, flags uint32) (fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+    return nil, 755 ,0
+}
+
+func (n *Node) Write(ctx context.Context, f fs.FileHandle, data []byte, off int64) (written uint32, errno syscall.Errno) {
+    fmt.Println(string(data))
+    fmt.Println(n.Path(n.Root()))
+    return uint32(len(data)), 0
+}
+func (n *Node) Setattr(ctx context.Context, f fs.FileHandle, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
+    return 0
 }
